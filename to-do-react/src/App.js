7e36ed74fd8c40c92a407353ab63c8c7b8c327f4
SergSmith64 <sergsmith2008@yandex.ru>
2020-04-03 22:31:50 +0400

    err: no decision yet

--- to-do-react/src/App.js
+++ to-do-react/src/App.js
@@ -5,25 +5,49 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+
+      {/* __ 1 __ */}
+      const Field = React.createClass({
+        getInitialState: function() {
+          return {text: "", items: []};
+        },
+      
+
+      {/* __ 6 __ */}
+      handleChange: function(e) {
+        this.setState({text: e.target.value})
+      },
+
+      {/* __ 10 __ */}
+      handleClick: function(e) {
+        const items = this.state.items;
+        const text = this.state.text;
+        items.push(text);
+        this.setState({text: "", items: items})
+      },
+
+      {/* __ 17 __ */}
+      render: function() {
+        const text = this.state.text;
+        const items = this.state.items;
+        return (
+          <div>
+            <input type="text" value={text} onChange={this.handleChange} />
+            <input type="button" value="add" onClick={this.handleClick} />
+            <ul>
+              {items.map(function(item)) {
+                return <li>{item}</li>;
+              } }
+            </ul>
+          </div>
+        );
+      }
+
     </div>
   );
 }
+
+})
+{/* React.render( <Field />, document.getElementById("root")) */}
 
 export default App;
--- to-do-react/src/index.js
+++ to-do-react/src/index.js
@@ -1,15 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+// _________
+
+const Item = React.createClass({
+  render: function() {
+  return <li>{this.props.value}</li>;
+  }
+})
+
+const Field = React.createClass({
+  getInitialState: function() {
+    return {text: "", items: []};
+  },
+
+handleChange: function(e) {
+  this.setState({text: e.target.value})
+},
+
+handleClick: function(e) {
+  const items = this.state.items;
+  const text = this.state.text;
+  items.push(text);
+  this.setState({text: "", items: items})
+},
+
+render: function() {
+  const text = this.state.text;
+  const items = this.state.items;
+  return (
+    <div>
+      <input type="text" value={text} onChange={this.handleChange} />
+      <input type="button" value="add" onClick={this.handleClick} />
+      <ul>
+        {items.map(function(item)) {
+          return <Item value={item} />;
+        } }
+      </ul>
+    </div>
+  );
+}
+
+</div>
+);
+}
+
+})
+React.render(
+  <Field />,
+  document.getElementById("root")
+);
+
+// ________
+
+{/* ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+); */}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
